84463e6c96211286282970f0088d58cdace2b895
khawlakha12 <kharbouchikhawla603@gmail.com>
2023-12-19 09:12:51 +0100

    home
    changemment de front home

--- index.php
+++ index.php
@@ -8,105 +8,196 @@ $ticketData = $ticketObj->selectData('tickets', '*', $ticketId);
 
 if ($ticketData) {
     ?>
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet"
-        href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/css/multi-select-tag.css">
-    <!-- component -->
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js" defer></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
-    <title>support </title>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        <!-- component -->
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js" defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+        <title>home</title>
+    </head>
 
-</head>
+    <body>
+        <!----------------------------------navbar------------------------->
+    <div>
+        <div x-data="{ sidebarOpen: false }" class="flex h-screen bg-gray-200">
+            <div :class="sidebarOpen ? 'block' : 'hidden'" @click="sidebarOpen = false"
+                class="fixed inset-0 z-20 transition-opacity bg-black opacity-50 lg:hidden"></div>
+            <div :class="sidebarOpen ? 'translate-x-0 ease-out' : '-translate-x-full ease-in'"
+                class="border-r-4 border-indigo-500 fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 transform bg-gradient-to-tr from-blue-50 via-violet-600 to-indigo-600 lg:translate-x-0 lg:static lg:inset-0">
+                <div class="flex items-center justify-center mt-8">
+                    <div class="flex items-center">
+                        <img class="h-10 w-28" src="img/logo H.T.S.png" alt="logo">
+                    </div>
+                </div>
 
-<body>
-
-    <section class="bg-indigo-500 dark:bg-gray-800 font-poppins">
-        <div class="max-w-6xl px-4 mx-auto" x-data="{open:false}">
-            <nav class="flex items-center justify-between py-3">
-                <a href="index.php"><img src="img/logo H.T.S.png" alt="" class="h-10 w-28"></a>
-                <div class="lg:hidden">
-                    <button
-                        class="flex items-center px-3 py-2 text-blue-200 border border-blue-200 rounded dark:text-gray-400 hover:text-blue-300 hover:border-blue-300 lg:hidden"
-                        @click="open =true">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                            class="bi bi-list" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd"
-                                d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                <nav class="mt-10">
+                    <a class="flex items-center px-6 py-2 mt-4 text-gray-50 bg-gray-700 bg-opacity-25" href="index.php">
+                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
                         </svg>
-                    </button>
-                </div>
-                <ul class="hidden lg:w-auto lg:space-x-12 lg:items-center lg:flex ">
-                    <li><a href="index.php"
-                            class="text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-200">Home</a>
-                    </li>
-                    <li><a href="support.php"
-                            class="text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-200">Support</a>
-                    </li>
-                    <li><a href=""
-                            class="text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-200">About</a>
-                    </li>
-                    <li><a href=""
-                            class="text-sm text-gray-200 dark:text-gray-300 hover:text-blue-200 dark:hover:text-blue-200"></a>
-                    </li>
 
-                </ul>
-                <div class="hidden lg:block">
-                    <a href=""
-                        class="inline-block px-4 py-3 mr-2 text-xs font-medium leading-none text-gray-100 border border-gray-200 rounded-full dark:text-gray-300 dark:border-gray-400 hover:bg-blue-200 dark:hover:text-gray-700 hover:text-gray-700">
-                        Profil
+                        <span class="mx-3">Home</span>
                     </a>
-                </div>
 
-            </nav>
-            <!-- Mobile Sidebar -->
-            <div class="absolute inset-0 z-10 h-screen p-3 text-gray-700 duration-500 transform shadow-md dark:bg-gray-800 bg-blue-50 w-80 lg:hidden lg:transform-none lg:relative"
-                :class="{'translate-x-0 ease-in-opacity-100' :open===true, '-translate-x-full ease-out opacity-0' : open===false}">
-                <div class="flex justify-between px-5 py-2">
-                <a href="index.php"><img src="img/logo H.T.S.png" alt="" class="h-10 w-28"></a>
-                    <button class="rounded-md hover:text-blue-300 lg:hidden dark:text-gray-400" @click="open=false">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                            class="bi bi-x-circle" viewBox="0 0 16 16">
-                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                            <path
-                                d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                    <a class="flex items-center px-6 py-2 mt-4 text-white hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                        href="support.php">
+                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                            </path>
                         </svg>
-                    </button>
-                </div>
-                <ul class="px-5 text-left mt-7">
-                    <li class="pb-3">
-                        <a href="index.php"
-                            class="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">home</a>
-                    </li>
-                    <li class="pb-3">
-                        <a href="support.php"
-                            class="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">support</a>
-                    </li>
-                    <li class="pb-3">
-                        <a href="" class="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400">About us</a>
-                    </li>
-                    <li class="pb-3">
-                        <a href="" class="text-sm text-gray-700 hover:text-blue-400 dark:text-gray-400"></a>
-                    </li>
 
-                </ul>
-                <div class="flex items-center mt-5 lg:hidden">
-                    <a href=""
-                        class="inline-block w-full px-4 py-3 mr-2 text-xs font-medium leading-none text-center text-gray-100 bg-blue-800 rounded-full dark:bg-blue-400 dark:text-gray-700 dark:border-gray-400 hover:bg-blue-200 dark:hover:text-gray-700 hover:text-gray-700">
-                        Profil
+                        <span class="mx-3">Support</span>
                     </a>
-                </div>
 
+                    <a class="flex items-center px-6 py-2 mt-4 text-white hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                        href="#">
+                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10">
+                            </path>
+                        </svg>
+
+                        <span class="mx-3">Tables</span>
+                    </a>
+
+                    <a class="flex items-center px-6 py-2 mt-4 text-white hover:bg-gray-700 hover:bg-opacity-25 hover:text-gray-100"
+                        href="#">
+                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z">
+                            </path>
+                        </svg>
+
+                        <span class="mx-3">Forms</span>
+                    </a>
+                </nav>
             </div>
-        </div>
-    </section> <!----------------------------------end navbar------------------------->
+            <div class="flex flex-col flex-1 "> 
+                <!-- 7yedt overflow hidden -->
+                <header class="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600">
+                    <div class="flex items-center">
+                        <button @click="sidebarOpen = true" class="text-gray-500 focus:outline-none lg:hidden">
+                            <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4 6H20M4 12H20M4 18H11" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round"></path>
+                            </svg>
+                        </button>
+
+                        <div class="relative mx-4 lg:mx-0">
+                            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                                <svg class="w-5 h-5 text-gray-500" viewBox="0 0 24 24" fill="none">
+                                    <path
+                                        d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                    </path>
+                                </svg>
+                            </span>
+
+                            <input class="w-32 pl-10 pr-4 rounded-md form-input sm:w-64 focus:border-indigo-600"
+                                type="text" placeholder="Search">
+                        </div>
+                    </div>
+
+                    <div class="flex items-center">
+                        <div x-data="{ notificationOpen: false }" class="relative">
+                            <button @click="notificationOpen = ! notificationOpen"
+                                class="flex mx-4 text-gray-600 focus:outline-none">
+                                <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+                                        stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round">
+                                    </path>
+                                </svg>
+                            </button>
+
+                            <div x-show="notificationOpen" @click="notificationOpen = false"
+                                class="fixed inset-0 z-10 w-full h-full" style="display: none;"></div>
+
+                            <div x-show="notificationOpen"
+                                class="absolute right-0 z-10 mt-2 overflow-hidden bg-white rounded-lg shadow-xl w-80"
+                                style="width: 20rem; display: none;">
+                                <a href="#"
+                                    class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
+                                    <img class="object-cover w-8 h-8 mx-1 rounded-full"
+                                        src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
+                                        alt="avatar">
+                                    <p class="mx-2 text-sm">
+                                        <span class="font-bold" href="#">Sara Salah</span> replied on the <span
+                                            class="font-bold text-indigo-400" href="#">Upload Image</span> artical . 2m
+                                    </p>
+                                </a>
+                                <a href="#"
+                                    class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
+                                    <img class="object-cover w-8 h-8 mx-1 rounded-full"
+                                        src="https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80"
+                                        alt="avatar">
+                                    <p class="mx-2 text-sm">
+                                        <span class="font-bold" href="#">Slick Net</span> start following you . 45m
+                                    </p>
+                                </a>
+                                <a href="#"
+                                    class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
+                                    <img class="object-cover w-8 h-8 mx-1 rounded-full"
+                                        src="https://images.unsplash.com/photo-1450297350677-623de575f31c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=334&amp;q=80"
+                                        alt="avatar">
+                                    <p class="mx-2 text-sm">
+                                        <span class="font-bold" href="#">Jane Doe</span> Like Your reply on <span
+                                            class="font-bold text-indigo-400" href="#">Test with TDD</span> artical . 1h
+                                    </p>
+                                </a>
+                                <a href="#"
+                                    class="flex items-center px-4 py-3 -mx-2 text-gray-600 hover:text-white hover:bg-indigo-600">
+                                    <img class="object-cover w-8 h-8 mx-1 rounded-full"
+                                        src="https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=398&amp;q=80"
+                                        alt="avatar">
+                                    <p class="mx-2 text-sm">
+                                        <span class="font-bold" href="#">Abigail Bennett</span> start following you . 3h
+                                    </p>
+                                </a>
+                            </div>
+                        </div>
+
+                        <div x-data="{ dropdownOpen: false }" class="relative">
+                            <button @click="dropdownOpen = ! dropdownOpen"
+                                class="relative block w-8 h-8 overflow-hidden rounded-full shadow focus:outline-none">
+                                <img class="object-cover w-full h-full"
+                                    src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=296&amp;q=80"
+                                    alt="Your avatar">
+                            </button>
+
+                            <div x-show="dropdownOpen" @click="dropdownOpen = false"
+                                class="fixed inset-0 z-10 w-full h-full" style="display: none;"></div>
+
+                            <div x-show="dropdownOpen"
+                                class="absolute right-0 z-10 w-48 mt-2 overflow-hidden bg-white rounded-md shadow-xl"
+                                style="display: none;">
+                                <a href="#"
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Profile</a>
+                                <a href="#"
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+                <!----------------------------------end navbar------------------------->
                 <?php foreach ($ticketData as $ticket): ?>
-                <div class="mb-4"></div>
+                    <div class="mb-4"></div>
                 
                 <div class='flex items-center justify-center '>
                     <div class="rounded-xl border p-5 shadow-md w-9/12 bg-white">
@@ -121,7 +212,7 @@ if ($ticketData) {
                             </div>
                             <div class="flex items-center space-x-8">
                                 <button
-                                    class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold"><?= $ticket['status'] ?></button>
+                                    class="rounded-2xl border bg-neutral-100 px-3 py-1 text-xs font-semibold">status</button>
                                 <div class="text-xs text-neutral-500">
                                     <?= $ticket['Date'] ?>
                                 </div>
